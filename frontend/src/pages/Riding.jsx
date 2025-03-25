fc237532de8d91e9119813f51fdbbf172d6fb76c
Kunal-bot235 <singhkunalkumar068@gmail.com>
2025-03-25 17:37:02 +0530

    Riding Page UI done

--- frontend/src/pages/Riding.jsx
+++ frontend/src/pages/Riding.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const WaitingForDriver = (props) => {
+const Riding = () => {
   return (
-    <div>
-      <h5 
-        onClick={()=>
-          props.WaitingForDriver(false)}
-        className='p-1 text-center w-[93%] absolute top-0'>
-        <i className='ri-arrow-down-wide-line text-3xl text-gray-200'></i>
-        </h5>
+    <div className='h-screen'>
+        <Link to='/home' className='fixed right-2 top-2 h-10 w-10 bg-white flex items-center justify-center rounded-full '>
+            <i className='text-lg font-medium ri-home-5-line'></i>
+        </Link>
+        <div className='h-1/2'>
+        <img className='h-full w-full object-cover' src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif"/>
+        </div>
+        <div className='h-1/2 p-4'>
         <div className='flex items-center justify-between'>
           <img className='h-20'src='https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1652995234/assets/92/8d4288-e896-4333-9bc2-c60c49f2a095/original/UberXL_Black_v2.png'></img>
           <div className='text-right'>
@@ -23,13 +25,7 @@ const WaitingForDriver = (props) => {
         
         
         <div className='w-full mt-5'>
-          <div className='flex items-center gap-2 p-3 border-b-2'>
-            <i className='text-lg ri-map-pin-2-fill'></i>
-            <div>
-              <h3 className='text-lg font-medium'>562/11-A</h3>
-              <p className='text-sm -mt-1 text-gray-600'>Kankariya Talab, Jamshedpur</p>
-            </div>
-          </div>
+          
 
 
           <div className='flex items-center gap-2 p-3 border-b-2'><i className='text-lg ri-map-pin-user-fill'></i>
@@ -44,8 +40,10 @@ const WaitingForDriver = (props) => {
             </div></div>
         </div>
         </div> 
+            <button className='w-full mt-5 bg-green-600 text-white font-semibold p-2 rounded-lg'>Make a Payment</button>
+        </div>
     </div>
   )
 }
 
-export default WaitingForDriver
+export default Riding
